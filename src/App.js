d9f5a68f97de6f7f1eff4b46f84b06f10f414eee
”Joonas <joonas.kinn@gmail.com>
2019-10-30 21:57:53 +0200

    add login functionality

--- src/App.js
+++ src/App.js
@@ -1,24 +1,95 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import blogService from './services/blogs'
+import loginService from './services/login'
+import Blog from './components/Blog'
 
-function App() {
+const App = () => {
+  const [user, setUser] = useState(null)
+  const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState("")
+  const [password, setPassword] = useState("")
+  const [errorMessage, setErrorMessage] = useState(null)
+
+  useEffect(() => {
+    blogService
+      .getAll().then(blogs => {
+        setBlogs(blogs)
+      })
+  }, [])
+  const handleLogin = async (event) => {
+    event.preventDefault()
+    try {
+      const user = await loginService.login({
+        username,
+        password
+      })
+      setUser(user)
+      setUsername("")
+      setPassword("")
+    } catch (e) {
+      setErrorMessage("wrong username or password")
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 4000)
+    }
+  }
+  const loginForm = () => {
+    return (
+      <div>
+        <h2>Log in</h2>
+        <form onSubmit={handleLogin}>
+          <div>
+            <span>username </span>
+            <input type="text"
+              name="Username"
+              value={username}
+              onChange={({ target }) => setUsername(target.value)} />
+          </div>
+          <div>
+            <span>password </span>
+            <input type="text" name="Password" value={password} onChange={({ target }) => setPassword(target.value)}></input>
+          </div>
+          <button type="submit">login</button>
+        </form>
+      </div>
+
+    )
+  }
+
+  const blogsForm = () => {
+    return (
+      <div>
+        <h2>blogs</h2>
+        {blogs.map((blog) => <Blog key={blog.id} blog={blog} />)}
+      </div>
+    )
+  }
+  const ErrorMessage = ({ message }) => {
+    const notificationStyle = {
+      border: "2px solid red",
+      color: "red",
+      backgroundColor: "#d3d3d3",
+      padding: "10px",
+      borderRadius: "5px",
+      display: "inline-block",
+      fontWeight: "bold"
+    }
+    if (message === null) {
+      return null
+    }
+    return (
+      <div style={notificationStyle}>
+        {message}
+      </div>
+    )
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Bloglist</h1>
+        <ErrorMessage message={errorMessage} />
       </header>
+      {user ? <div> <p>{user.name} logged in</p>{blogsForm()} </div> : loginForm()}
     </div>
   );
 }
